3e35b930b6527c7a0d5cc240c4f2b6a3ff2e4beb
Kuldeep83943 <134301330+Kuldeep83943@users.noreply.github.com>
2024-04-25 22:26:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+// let currentDate = new Date();
+// let output = document.getElementById("timer");
+// output.innerText=currentDate;
+
+ function updateTime() {
+            // Get the current date and time
+            var now = new Date();
+
+            // Format the date and time
+            var dateString = now.toLocaleDateString();
+            var timeString = now.toLocaleTimeString();
+
+            // Update the content of the p tag
+            document.getElementById('timer').textContent = dateString + ', ' + timeString;
+        }
+
+        // Call updateTime initially to display the time immediately
+        updateTime();
+
+        // Update the time every second
+        setInterval(updateTime, 1000);
